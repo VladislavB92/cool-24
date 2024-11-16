4c3aeb8e69c84418d71441b283027d224ae2551a
vlad <vladislavs@codnity.com>
2024-11-16 14:45:58 +0200

    Add course model

--- students/models.py
+++ students/models.py
@@ -11,3 +11,11 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=32, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
